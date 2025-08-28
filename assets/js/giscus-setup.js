06828cacc9543933b78f0b5672a84a7ea45836e5
Didgety <jnbv00@gmail.com>
2025-08-28 12:46:16 +0000

    Deploying to gh-pages from @ Didgety/didgety.github.io@77927a6e72b91552f7c6af1266dd32c97081cd92 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Didgety/didgety.github.io-new",
+    "data-repo": "Didgety/didgety.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
